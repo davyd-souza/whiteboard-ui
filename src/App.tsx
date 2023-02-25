81bd4b799dc577152c4f2669054bc7441a14e2ec
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-25 01:08:55 -0300

    feat: implement board with nodes, toolbar and connections

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,80 @@
-import ReactFlow from 'reactflow'
+// DEPENDENCY
+import { useCallback } from 'react'
+import ReactFlow, {
+  Background,
+  Controls,
+  Node,
+  ConnectionMode,
+  useEdgesState,
+  Connection,
+  addEdge,
+  useNodesState,
+} from 'reactflow'
 import 'reactflow/dist/style.css'
 
+// COMPONENT
+import { Square } from './components/nodes/Square'
+import { DefaultEdge } from './components/edges/DefaultEdge'
+import * as Toolbar from '@radix-ui/react-toolbar'
+
+// STYLE
+import { zinc } from 'tailwindcss/colors'
+
+// VARIABLE
+const NODE_TYPES = {
+  square: Square,
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
+}
+
 export function App() {
-  return <h1>Whiteboard UI</h1>
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  const [nodes, setNodes, onNodesChange] = useNodesState([])
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges((prevEdges) => addEdge(connection, prevEdges))
+  }, [])
+
+  function addSquareNode() {
+    setNodes((prevNodes) => [
+      ...prevNodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+          x: 750,
+          y: 350,
+        },
+        data: {},
+      },
+    ])
+  }
+
+  return (
+    <div className='h-screen w-screen'>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        edgeTypes={EDGE_TYPES}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{ type: 'default' }}
+      >
+        <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
+      </ReactFlow>
+
+      <Toolbar.Root className='fixed bottom-10 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+        <Toolbar.Button
+          onClick={addSquareNode}
+          className='w-32 h-32 bg-yellow-400 mt-6 rounded transition-transform hover:-translate-y-3'
+        />
+      </Toolbar.Root>
+    </div>
+  )
 }
