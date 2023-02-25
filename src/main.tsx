47bc8eada0920cb8f5eb642aa8f354b40fc6ca76
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-25 01:07:23 -0300

    style: organize imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+// DEPENDENCY
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// COMPONENT
 import { App } from './App'
 
+// STYLE
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
